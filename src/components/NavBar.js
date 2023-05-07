7ca29a06b1104ea36ccf0bb3304ff6a3808aa931
KamiyaGaikwad <79163529+KamiyaGaikwad@users.noreply.github.com>
2023-05-07 20:14:03 +0530

    Styles: Made navbar responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 
-export function NavBar({isnavhidden}){
+export function NavBar(){
     const getActiveStyles = ({isActive}) =>({
       color:isActive?'red':'black',
       fontWeight:isActive?'600':'400',
       borderRight:isActive?'4px solid #313134':'none'
     });
+
+    const getActiveStylesMobile = ({isActive}) =>({
+      color:isActive?'red':'black',
+      fontWeight:isActive?'600':'400',
+      borderBottom:isActive?'4px solid #313134':'none'
+    });
     return(
-        <nav className={isnavhidden?'navbar':'navbar show'}>
+      <>
+        <nav className="navbar">
           <NavLink to="/" style={getActiveStyles}>Inbox</NavLink>
           <NavLink to="/spam" style={getActiveStyles}>Spam</NavLink>
           <NavLink to="/trash" style={getActiveStyles}>Trash</NavLink>
         </nav>
+
+        <nav className='nav-mobile'>
+          <NavLink to="/" style={getActiveStylesMobile}>Inbox</NavLink>
+          <NavLink to="/spam" style={getActiveStylesMobile}>Spam</NavLink>
+          <NavLink to="/trash" style={getActiveStylesMobile}>Trash</NavLink>
+        </nav>
+      </>
     )
   }
